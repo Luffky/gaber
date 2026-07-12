6f5f33b900dcd778a48ea212ee298d4beb18cd7f
gabriel <fukaiyu.214@bytedance.com>
2026-07-12 12:23:03 +0800

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by ByteDance on 2026/7/12.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
